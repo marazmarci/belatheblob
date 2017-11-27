215a69feb4a12bfe718942fcdc5eb69a404a9a62
marazmarci <marazmarci@gmail.com>
2017-11-28 00:56:29 +0100

    added @Prog3HF annotation

--- core/src/hu/marazmarci/belatheblob/states/Menu.java
+++ core/src/hu/marazmarci/belatheblob/states/Menu.java
@@ -1,6 +1,31 @@
 package hu.marazmarci.belatheblob.states;
 
-public class Menu {
+import hu.marazmarci.belatheblob.handlers.GameStateManager;
+
+public class Menu extends GameState {
 
 
+    Menu(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput(float dt) {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
