0d05357607d0b1a0bbda4cc0e66ae83bea31a654
marazmarci <marazmarci@gmail.com>
2017-11-29 15:21:37 +0100

    added ButtonAction interface

---
+++ core/src/hu/marazmarci/belatheblob/gui/ButtonAction.java
@@ -0,0 +1,10 @@
+package hu.marazmarci.belatheblob.gui;
+
+import hu.marazmarci.belatheblob.Prog3HF_JavaDoc_TODO;
+
+@Prog3HF_JavaDoc_TODO
+public interface ButtonAction {
+
+    void run(Button button);
+
+}
