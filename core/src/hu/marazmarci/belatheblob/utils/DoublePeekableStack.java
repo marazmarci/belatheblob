46d470070ef3b0e5f09e91481fde037f7235912f
marazmarci <marazmarci@gmail.com>
2017-11-29 15:19:17 +0100

    annotateed with @Prog3HF_JavaDoc_TODO

--- core/src/hu/marazmarci/belatheblob/utils/DoublePeekableStack.java
+++ core/src/hu/marazmarci/belatheblob/utils/DoublePeekableStack.java
@@ -1,11 +1,13 @@
 package hu.marazmarci.belatheblob.utils;
 
 import hu.marazmarci.belatheblob.Prog3HF;
+import hu.marazmarci.belatheblob.Prog3HF_JavaDoc_TODO;
 
 import java.util.EmptyStackException;
 import java.util.Stack;
 
 @Prog3HF
+@Prog3HF_JavaDoc_TODO
 public class DoublePeekableStack<T> extends Stack<T> {
 
     public synchronized T peekSecond() {
