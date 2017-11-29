cbf6571d50c493230cbfe2f6af1a2708c74226ec
marazmarci <marazmarci@gmail.com>
2017-11-29 21:12:25 +0100

    added Javadoc

--- core/src/hu/marazmarci/belatheblob/states/GameLevelScreen.java
+++ core/src/hu/marazmarci/belatheblob/states/GameLevelScreen.java
@@ -1,11 +1,12 @@
 package hu.marazmarci.belatheblob.states;
 
 import hu.marazmarci.belatheblob.Prog3HF;
-import hu.marazmarci.belatheblob.Prog3HF_JavaDoc_TODO;
 import hu.marazmarci.belatheblob.handlers.GameStateManager;
 
+/**
+ * Egy olyan játékállapotot reprezentál, ami konkrétan már egy játékpálya.
+ */
 @Prog3HF
-@Prog3HF_JavaDoc_TODO
 public abstract class GameLevelScreen extends GameStateScreen {
 
     // TODO level-specific attributes
